15ae8e8e99f1bbc0f82e6250e5c3a82a231ec01b
Abu Saadat <103253429+SAADAT-Abu@users.noreply.github.com>
2024-11-05 17:09:35 +0100

    Update create_bedmeth.R

--- R/create_bedmeth.R
+++ R/create_bedmeth.R
@@ -17,12 +17,14 @@
 #' bed_data_v2 <- create_bedmeth(version = "v2")
 #' @export
 
-library(IlluminaHumanMethylation450kanno.ilmn12.hg19)
-library(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
-library(IlluminaHumanMethylationEPICv2anno.20a1.hg38)
-library(tidyverse)
-
 create_bedmeth <- function(version = "v1") {
+ 
+  # Load required library
+  library(IlluminaHumanMethylation450kanno.ilmn12.hg19)
+  library(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
+  library(IlluminaHumanMethylationEPICv2anno.20a1.hg38)
+  library(tidyverse)
+  
   if (version == "v1") {
     anno <- getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
     bedmeth <- as.data.frame(anno) %>%
