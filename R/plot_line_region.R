23451d14dbbf9095a68c85d2376a50b5c7ba67f3
ngsFC <francesco.cecerengs@gmail.com>
2024-11-15 22:36:37 +0100

    add line plot

---
+++ R/plot_line_region.R
@@ -0,0 +1,39 @@
+plot_line_region <- function(significantDMPs, ICRcpg, ICR, sampleInfo) {
+  regionDMPs <- significantDMPs[significantDMPs$ICR == ICR, ]
+  if (nrow(regionDMPs) == 0) {
+    stop("No significant DMPs found for the specified ICR.")
+  }
+  
+  regionStart <- min(regionDMPs$start)
+  regionEnd <- max(regionDMPs$end)
+  
+  regionCpGs <- ICRcpg[ICRcpg$cstart >= regionStart & ICRcpg$cstart <= regionEnd, ]
+  if (nrow(regionCpGs) == 0) {
+    stop("No CpGs found in the specified ICR region.")
+  }
+  
+  methylationData <- regionCpGs[, 1:(ncol(regionCpGs) - 4)]
+  annotationData <- regionCpGs[, (ncol(regionCpGs) - 3):ncol(regionCpGs)]
+  
+  methylationData$cstart <- annotationData$cstart
+  methylationDataLong <- reshape2::melt(methylationData, id.vars = "cstart", variable.name = "Sample", value.name = "Methylation")
+  
+  methylationDataLong$Type <- rep(sampleInfo, each = nrow(methylationData))
+  methylationDataLong$IsDMP <- methylationDataLong$cstart %in% regionDMPs$cstart
+  
+  plot <- ggplot(methylationDataLong, aes(x = cstart, y = Methylation, group = Sample, color = Type)) +
+    geom_line(aes(linetype = Type), alpha = 0.7) +  # Line plot for each sample
+    geom_point(data = methylationDataLong[methylationDataLong$IsDMP, ], aes(shape = IsDMP), size = 3) +  # Highlight DMPs
+    geom_vline(data = data.frame(x = regionDMPs$cstart), aes(xintercept = x), color = "lightgrey", linetype = "dashed") +
+    scale_color_manual(values = c("Control" = "grey", "Case" = "red")) +
+    scale_linetype_manual(values = c("Control" = "solid", "Case" = "solid")) +
+    labs(
+      title = paste("Methylation in Region:", ICR),
+      x = "CpG Coordinates",
+      y = "Methylation Value"
+    ) +
+    theme_minimal()
+  
+  return(plot)
+}
+
