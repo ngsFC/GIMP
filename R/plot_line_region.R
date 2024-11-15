0c7469a18639200daeccfce46602703c09457ed0
ngsFC <francesco.cecerengs@gmail.com>
2024-11-15 23:07:22 +0100

    fix dot size and color

--- R/plot_line_region.R
+++ R/plot_line_region.R
@@ -14,16 +14,16 @@ plot_line_region <- function(significantDMPs, ICRcpg, ICR, sampleInfo) {
   
   methylationData <- regionCpGs[, 1:(ncol(regionCpGs) - 4)]
   annotationData <- regionCpGs[, (ncol(regionCpGs) - 3):ncol(regionCpGs)]
-  methylationData$cstart <- annotationData$cstart
-  methylationDataLong <- reshape2::melt(methylationData, id.vars = "cstart", variable.name = "Sample", value.name = "Methylation")
   
+  ìmethylationData$cstart <- annotationData$cstart
+  methylationDataLong <- reshape2::melt(methylationData, id.vars = "cstart", variable.name = "Sample", value.name = "Methylation")
   methylationDataLong$Type <- rep(sampleInfo, each = nrow(methylationData))
   methylationDataLong$IsDMP <- methylationDataLong$cstart %in% regionDMPs$cstart
   
   plot <- ggplot(methylationDataLong, aes(x = cstart, y = Methylation, group = Sample, color = Type)) +
     geom_line(aes(linetype = Type), alpha = 0.5) +  # Adjusted alpha for lines to enhance comparability
-    geom_point(aes(shape = IsDMP, color = Type), size = 1.5, color = "grey") +  # Smaller grey points for all CpGs
-    geom_point(data = methylationDataLong[methylationDataLong$IsDMP, ], size = 2.5, color = "black") +  # Slightly larger black points for DMPs
+    geom_point(data = methylationDataLong[!methylationDataLong$IsDMP, ], size = 1, color = "grey") +  # Smaller grey points for non-DMP CpGs
+    geom_point(data = methylationDataLong[methylationDataLong$IsDMP, ], size = 1.5, color = "black") +  # Slightly larger black points for DMPs
     geom_vline(data = data.frame(x = regionDMPs$cstart), aes(xintercept = x), color = "lightgrey", linetype = "dashed") +
     scale_color_manual(values = c("Control" = "grey", "Case" = "red")) +
     scale_linetype_manual(values = c("Control" = "solid", "Case" = "solid")) +
@@ -34,7 +34,6 @@ plot_line_region <- function(significantDMPs, ICRcpg, ICR, sampleInfo) {
     ) +
     theme_minimal()
   
-  # Return the plot
   return(plot)
 }
 
